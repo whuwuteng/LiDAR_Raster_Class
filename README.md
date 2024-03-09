d175eca065dfaf0e142871cc0b5e47af416b0ee9
whuwuteng <whuwuteng@gmail.com>
2024-03-09 22:33:37 +0100

    upeate

---
+++ README.md
@@ -0,0 +1,68 @@
+# LiDARHD to raster image
+
+## Use the sampling to create raster image with classification property
+
+To raster the LiDAR point cloud, we can only save one label for each pixel, because **building** is the important part in the project(GENESE). So if in the pixel, there is a building point, we set this pixel to building label.
+
+The out put file is a Tiff file with tfw, so that this file can have the geo-information, and this can be open with QGIS. 
+
+## Dependency
+
+The code depends on my code project :
+
+### [OpenImageoiioLib](https://github.com/whuwuteng/OpenImageoiioLib)
+
+Because the code depends on  [OpenImageIO](https://sites.google.com/site/openimageio/home) , because we only write Tiff format, so it is easy to install.
+
+```
+# libtiff
+sudo apt-get update && apt-get install -y \
+		    build-essential \
+		    make \ 
+            git \
+			automake \
+			libtool \
+			zlib1g-dev \
+			libboost-all-dev \
+			libtiff-dev \
+			libpng-dev \
+			libjpeg-dev \
+			libeigen3-dev
+
+# code from OpenImageIO
+git clone --recurse https://github.com/OpenImageIO/oiio.git && \
+    cd oiio && \
+    mkdir build && \
+    cd build && \
+    cmake -DOIIO_BUILD_TESTS=0 -DUSE_OPENGL=0 -DUSE_QT=0 -DUSE_PYTHON=0 -DUSE_FIELD3D=0 -DUSE_FFMPEG=0 -DUSE_OPENJPEG=1 -DUSE_OPENCV=0 -DUSE_OPENSSL=0 -DUSE_PTEX=0 -DUSE_NUKE=0 -DUSE_DICOM=0 ../ && \
+    make -j16 && 
+    sudo make install
+```
+
+### [WuLasLib](https://github.com/whuwuteng/WuLasLib)
+
+This part is already in the code repository, so it doesn't matter.
+
+## Example
+
+There is an example using the LiDARHD :
+
+```
+ cd LiDAR_Raster_Class
+ mkdir build
+ cd build
+ cmake ..
+ make -j4
+ 
+ ./ConvertLasImage '/home/whuwuteng/AI4GEO-Toulouse-IMAGE/ALS/LHD_FXX_0574_6277_PTS_C_LAMB93_IGN69.copc.laz' 0.15 '/home/whuwuteng/AI4GEO-Toulouse-IMAGE/ALS/raster2.tif'
+```
+
+The example is shown (convert to png) :
+
+| <img src="/figures/raster2.png" width="700" alt="*Raster image*" /> |
+| :----------------------------------------------------------: |
+|                *Raster image*                |
+
+
+## MAINTENANCE
+If you think you have any problem, contact [Teng Wu]<whuwuteng@gmail.com>
--- example/lidar_raster.cpp
+++ example/lidar_raster.cpp
@@ -11,13 +11,19 @@
 #include "WuLasLib.h"
 #include "OpenImageoiio.h"
 
-// compare to https://www.cloudcompare.org/doc/wiki/index.php/Rasterize
-// for TFW file gdal_translate -co "TFW=YES"  raster.tif raster2.tif
 int main(int argc, char const *argv[])
 {
-	char szSrcLas[512] = "/home/whuwuteng/AI4GEO-Toulouse-IMAGE/ALS/LHD_FXX_0574_6277_PTS_C_LAMB93_IGN69.copc.laz";
+    if (argc != 4){
+        std::cout << "ConvertLasImage Las GSD Tif" << std::endl;
+        return -1;
+    }
 
-	double gridsize = 0.15;
+    char szSrcLas[512] = { 0 };
+    strcpy(szSrcLas, argv[1]);
+
+    // LiDARHD
+	//double gridsize = 0.15;
+    double gridsize = atof(argv[2]);
 
 	CWuLasLib srcLas;
 	srcLas.Open(szSrcLas);
@@ -25,17 +31,12 @@ int main(int argc, char const *argv[])
 	int nPoints = srcLas.GetPtNum();
 
     std::cout << "Point number: " << nPoints << std::endl;
-    // keep as much as information
+
 	LasPointClass * pFullPoint = new LasPointClass[nPoints];
 	memset(pFullPoint, 0, sizeof(LasPointClass) * nPoints);
     
     srcLas.ReadLas(pFullPoint, nPoints);
 	srcLas.Close();
-
-    //return -1;
-    
-    // debug
-    // nPoints = 10000;
     
 	double minx = pFullPoint[0].x;
     double maxx = pFullPoint[0].x;
@@ -61,12 +62,9 @@ int main(int argc, char const *argv[])
     std::cout << "Rows:" << nRows << std::endl;
     std::cout << "Cols:" << nCols << std::endl;
 
-    // we can use a 2D version, but the 3D version is already exist
 	unsigned char * pLabelClass = new unsigned char[nRows * nCols];
     memset(pLabelClass, 0, sizeof(unsigned char) * nRows * nCols);
     
-    // image and point cloud the system is different
-    // refer to http://geotiff.maptools.org/spec/geotiff2.5.html
     for (int i = 0; i < nPoints; ++i){
 		int x = int((pFullPoint[i].x - minx)/gridsize);
 		int y = int((pFullPoint[i].y - miny)/gridsize);
@@ -79,19 +77,7 @@ int main(int argc, char const *argv[])
     }
     
     std::cout << "Convert to raster image..." << std::endl;
-    // refer to https://docs.qgis.org/3.34/en/docs/user_manual/working_with_point_clouds/point_clouds.html
-    // set the point cloud classification renderer
-    // generate the image
-    // 1 170 170 170
-    // 2 170 85 0
-    // 3 0 170 170
-    // 4 85 255 85
-    // 5 0 170 0
-    // 6 255 85 85
-    // 17 85 85 255
-    // 64 232 113 141
-    // 65 141 90 153
-    // 67 243 166 178
+
     unsigned char * pRasterImage = new unsigned char[nRows * nCols * 3];
 	memset(pRasterImage, 0, sizeof(unsigned char) * nRows * nCols * 3);
 
@@ -152,7 +138,9 @@ int main(int argc, char const *argv[])
 
 	// write the geotif file
 	std::cout << "Save image..." << std::endl;
-	char szTarTif[512] = "/home/whuwuteng/AI4GEO-Toulouse-IMAGE/ALS/LHD_FXX_0574_6277_PTS_C_LAMB93_IGN69.copc.tif";
+    
+    char szTarTif[512] = { 0 };
+    strcpy(szTarTif, argv[3]);
     
     SaveImg(szTarTif, pRasterImage, nRows, nCols, 3);
     delete []pRasterImage;                  pRasterImage = NULL;
